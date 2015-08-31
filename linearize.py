7fc45bc542920de8285710fb4648c87c4ab10a33
nanotone <yang.yang@enigma.io>
2015-08-31 12:48:56 -0400

    Use argparse in linearize and add orig exif path option

--- linearize.py
+++ linearize.py
@@ -1,9 +1,10 @@
 import subprocess
-import sys
 
 import cv2
 import lensfunpy
 
+
+class EXIFError(Exception): pass
 
 def get_exif(path, key, numeric=False):
     """Use exiftool to retrieve EXIF values from an image."""
@@ -12,7 +13,7 @@ def get_exif(path, key, numeric=False):
         args.insert(1, '-n')
     output = subprocess.check_output(args).strip()
     if ':' not in output:
-        raise KeyError("%s has no EXIF data for %s" % (path, key))
+        raise EXIFError("%s has no EXIF data for %s" % (path, key))
     return output.split(':')[1].strip()
 
 
@@ -25,9 +26,14 @@ def get_cam_lens(path, lfdb):
     if lens_maker == 'Canon' and 'EF' in lens_model and 'EF ' not in lens_model:
         lens_model = lens_model.replace('EF', 'EF ')
 
-    cam = lfdb.find_cameras(cam_maker, cam_model)[0]
-    lens = lfdb.find_lenses(cam, lens_maker, lens_model)[0]
-    return (cam, lens)
+    cams = lfdb.find_cameras(cam_maker, cam_model)
+    if not cams:
+        raise EXIFError("Camera %r made by %r not found in lensfun" % (cam_model, cam_maker))
+    cam = cams[0]
+    lenses = lfdb.find_lenses(cam, lens_maker, lens_model)
+    if not lenses:
+        raise EXIFError("Lens %r made by %r not found in lensfun" % (lens_model, lens_maker))
+    return (cam, lenses[0])
 
 
 def get_modifier(path, lfdb):
@@ -51,9 +57,19 @@ def get_map_coords(path, distance=10):
 
 
 if __name__ == '__main__':
-    src_path, dst_path = sys.argv[1:]
-    undist_coords = get_map_coords(src_path, distance=10)
+    import argparse
+    import sys
+    parser = argparse.ArgumentParser(description="Use lensfun to fix lens distortion.")
+    parser.add_argument('src')
+    parser.add_argument('dst')
+    parser.add_argument('--exif-src', help="path to file with original EXIF data")
+    args = parser.parse_args()
 
-    im = cv2.imread(src_path)
+    try:
+        undist_coords = get_map_coords(args.exif_src or args.src, distance=10)
+    except EXIFError as e:
+        sys.exit(e.message)
+
+    im = cv2.imread(args.src)
     im_undistorted = cv2.remap(im, undist_coords, None, cv2.INTER_LANCZOS4)
-    cv2.imwrite(dst_path, im_undistorted)
+    cv2.imwrite(args.dst, im_undistorted)
