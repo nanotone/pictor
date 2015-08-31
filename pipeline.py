41ce7533a994123327adeb2e8007f6c56330f9c6
nanotone <yang.yang@enigma.io>
2015-08-31 14:30:06 -0400

    Allow skipping undistortion, and tidy up jpg/exif logic

--- pipeline.py
+++ pipeline.py
@@ -6,9 +6,16 @@ orig_path = None
 dst_dir = 'tmp'
 pictor = ''
 
+
+def check_call(*args):
+    print
+    print '   '.join(args)
+    subprocess.check_call(args)
+
+
 exiftool = True
 try:
-    subprocess.check_call(['exiftool', '-ver'])
+    check_call('exiftool', '-ver')
 except subprocess.CalledProcessError:
     exiftool = False
     print "WARNING: exiftool not found"
@@ -19,11 +26,6 @@ def dst_path(root, suffix):
 
 def pictor_path(exe):
     return os.path.join(pictor, exe)
-
-def check_call(*args):
-    print
-    print '   '.join(args)
-    subprocess.check_call(args)
 
 
 def make_flat(root):
@@ -40,20 +42,20 @@ def make_flatnorm(root):
         check_call('python', pictor_path('flat-norm.py'), src, dst)
     return dst
 
-def make_flattened(root):
+def make_flattened(root, ext='png'):
     src = make_flatnorm(root)
-    dst = dst_path(root, 'flattened.png')
+    dst = dst_path(root, 'flattened.%s' % ext)
     if not os.path.isfile(dst):
         check_call('go', 'run', pictor_path('flat-fix.go'), orig_path, src, dst)
     return dst
 
-def make_linear(root, png=False):
+def make_linear(root, ext='png'):
     src = make_flattened(root)
-    dst = dst_path(root, 'linear.%s' % ('png' if png else 'jpg'))
+    dst = dst_path(root, 'linear.%s' % ext)
     if not os.path.isfile(dst):
         if not exiftool:
             sys.exit("exiftool, which is required for linearize.py, could not be found")
-        check_call('python', pictor_path('linearize.py'), src, dst)
+        check_call('python', pictor_path('linearize.py'), src, dst, '--exif-src', orig_path)
     return dst
 
 
@@ -63,6 +65,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Send images through the flattening and undistorting pipeline.")
     parser.add_argument('--dst', default='tmp', help="directory ")
     parser.add_argument('--png', action='store_true', help="output results as PNG for further processing (no EXIF)")
+    parser.add_argument('--skip-undistort', action='store_true', help="skip lensfun undistortion step")
     parser.add_argument('src', nargs='+', help="images to process")
     args = parser.parse_args()
 
@@ -73,9 +76,16 @@ if __name__ == '__main__':
         sys.exit("%s is not a directory." % args.dst)
 
     dst_dir = args.dst
+    output_ext = 'png' if args.png else 'jpg'
     for orig_path in args.src:
         (head, tail) = os.path.split(orig_path)
         (root, ext) = os.path.splitext(tail)
-        dst = make_linear(root, png=args.png)
-        if exiftool and not args.png:
-            check_call('exiftool', '-overwrite_original', '-tagsFromFile', orig_path, dst)
+        try:
+            if args.skip_undistort:
+                dst = make_flattened(root, ext=output_ext)
+            else:
+                dst = make_linear(root, ext=output_ext)
+            if exiftool and not args.png:
+                check_call('exiftool', '-overwrite_original', '-tagsFromFile', orig_path, dst)
+        except subprocess.CalledProcessError as e:
+            sys.exit("Command returned non-zero exit status.")
